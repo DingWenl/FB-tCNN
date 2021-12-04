0331b0463c7a7aeb71c45607373a1149c125999f
Wenlong Ding <86034682+DingWenl@users.noreply.github.com>
2021-12-04 20:09:42 +0800

    Update data_generator.py

--- FB-tCNN/data_generator.py
+++ FB-tCNN/data_generator.py
@@ -52,7 +52,7 @@ def val_datagenerator(batchsize,train_data1,train_data2,train_data3,train_data4,
         for i in range(batchsize):
             k = sample(val_list, 1)[0]
             y_data = y_label[k]-1
-            time_start = random.randint(35,int(1000-win_train))
+            time_start = random.randint(35,int(1000+35-win_train))
             x1 = int(start_time[k]/down_sample)+time_start
             x2 = int(start_time[k]/down_sample)+time_start+win_train
             
